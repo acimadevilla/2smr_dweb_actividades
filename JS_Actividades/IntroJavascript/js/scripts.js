684dad66ccf797cd2d1e78dd6bb31e17a4e38194
Alejandro Cimadevilla <alejandro.cimadevilla@gmail.com>
2026-05-06 07:54:08 +0200

    Intro Javascript day1

---
+++ JS_Actividades/IntroJavascript/js/scripts.js
@@ -0,0 +1,22 @@
+console.log("Este es un mensaje desde javascript");
+
+let nombre="Alejandro";  
+let edad = 10;
+let aprobado = true;
+
+const pi = 4.1416;
+
+nombre = "Juan";
+
+if ( edad >= 18  && edad <= 30) {
+
+    console.log(nombre+" puede venir a la fiesta porque eres joven y cool");    
+    
+} else {
+    console.log(nombre+" eres o muy joven o un pureta ("+edad+")");
+}
+
+
+
+
+
